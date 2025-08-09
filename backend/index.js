f4a43e8d66855db339d33541c840408a914052a6
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-08-09 21:54:01 +0530

    lect 44

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,13 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello Ubed!')
-})
+//available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Ubed!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const { Mongoose, default: mongoose } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: date,
+    default: date.now,
+  },
+});
+
+module.exports = mongoose.model('user', UserSchema)
