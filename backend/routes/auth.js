6080c6d2a2c26e91328f9728d579a8b91c6fb2b0
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-08-10 22:21:43 +0530

    lect 46 complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,18 +7,29 @@ const { query, validationResult, body } = require("express-validator");
 //create  a user using: POST "/api/auth/". dosen't require Auth
 router.post(
   "/",
-  [body("name").isLength({min: 3}),
-     body("email").isEmail()],
+  [
+    body("name", "enter a valid name ").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password at least 5 characters ").isLength({ min: 5 }),
+  ],
+
   (req, res) => {
     // get and post issue solve krrrr IMPPPPPPPPPPPPPPPPPp
     // console.log(req.body);
     // const user = User(req.body);
     // user.save();
-    const errors = validationResult(req)
-    if (!errors.isEmpty()){
-      return res.this.status(400).json({errors: errors.array()})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    }).then((user) => res.json(user))
+    .catch(err=> {console.log(err);
+      res.json({error:'please enter a unique value for email', message: err.message})})
+    // res.send(req.body);
   }
 );
 
