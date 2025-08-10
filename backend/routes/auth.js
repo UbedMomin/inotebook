a1a027ceed76b73215d31c559b4fe9d94069d7d9
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-08-10 21:47:52 +0530

    lect 46 just half

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,13 +2,24 @@ const express = require("express");
 const { model } = require("mongoose");
 const User = require("../models/User");
 const router = express.Router();
+const { query, validationResult, body } = require("express-validator");
 
 //create  a user using: POST "/api/auth/". dosen't require Auth
-router.get("/", (req, res) => { // get and post issue solve krrrr IMPPPPPPPPPPPPPPPPPp
-  console.log(req.body);
-  const user = User(req.body);
-  user.save()
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [body("name").isLength({min: 3}),
+     body("email").isEmail()],
+  (req, res) => {
+    // get and post issue solve krrrr IMPPPPPPPPPPPPPPPPPp
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save();
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+      return res.this.status(400).json({errors: errors.array()})
+    }
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
